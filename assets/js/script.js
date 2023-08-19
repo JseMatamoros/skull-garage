9660bc32d9ee6b476c2e4868b5a4ebdacbff2144
JseMatamoros <josematamoros916@gmail.com>
2023-08-19 01:29:09 -0400

    otro commit pero mas relacionado a ajustes delnavbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-// navbar
-$(document).ready(function() {
-    $(window).scroll(function() {
-      const navbar = $(".navbar");
-      if ($(window).scrollTop() > 0) {
-        navbar.addClass("expanded");
-      } else {
-        navbar.removeClass("expanded");
-      }
-    });
-  });
 // testing wathsapp web and mobile
 $(document).ready(function() {
     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
